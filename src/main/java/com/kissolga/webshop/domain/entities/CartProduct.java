5aac307d35b891d1c06995c8447841f6e96122d3
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2025-01-29 21:06:03 +0100

    fixed object relational mapping

--- src/main/java/com/kissolga/webshop/domain/entities/CartProduct.java
+++ src/main/java/com/kissolga/webshop/domain/entities/CartProduct.java
@@ -1,6 +1,5 @@
 package com.kissolga.webshop.domain.entities;
 
-import com.kissolga.webshop.domain.dtos.ProductDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,12 +13,6 @@ public class CartProduct {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    //mit kossek mibe?
-    //mindegyik cart product egy termek a kosaramban
-    //egy produvctot tartalmazzon
-    //egy productnak lehet tobb cartproductja
-    //egy a többhöz - Productba
-
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
@@ -28,4 +21,3 @@ public class CartProduct {
 
     private double price;
 }
-//TODO
