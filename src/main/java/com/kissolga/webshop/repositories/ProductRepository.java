4ef97d5d81e9014db26af6fbe0231f92d86079f2
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2025-03-12 20:10:05 +0100

    create repositories

---
+++ src/main/java/com/kissolga/webshop/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.kissolga.webshop.repositories;
+
+import com.kissolga.webshop.domain.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
